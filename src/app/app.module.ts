0132de56482000df1c6e86746f7b1c6a52a5cde0
ruddyyhit <47591711+ruddyyhit@users.noreply.github.com>
2019-07-03 18:21:19 +0530

    basic component with databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+// selector: `.app-server`,
+selector: 'app-server',
+templateUrl: './server.Component.html',
+styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
